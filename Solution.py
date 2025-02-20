7c72e1b84e5251f9cfe0a46f3d10d63ae6fdc8d8
AIDAN-ASHTON <aidanashton35@gmail.com>
2025-02-20 20:02:32 +0000

    Added validation methods for user input

--- Solution.py
+++ Solution.py
@@ -1,8 +1,8 @@
 #class to represent the plateau for the rovers to traverse
 class Matrix:
     def __init__(self, height, width):
-        self.height = height
-        self.width = width
+        self.height = int(height)
+        self.width = int(width)
 
     #function checks if given coordinates are on the plateau
     def is_valid_position(self, x, y):
@@ -11,10 +11,11 @@ class Matrix:
 #class to represent the rovers
 class Rover:
     def __init__(self, x, y, direction, matrix):
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
         self.direction = direction
         self.matrix = matrix
+        self.instructions = []
 
     #function to move the rover
     def move(self):
@@ -67,12 +68,71 @@ class Solution:
         return [(rover.x, rover.y, rover.direction) for rover in self.rovers]
     
 if __name__ == "__main__":
-    matrix = Matrix(5, 5)
 
-    rovers = [Rover(1, 2, 'N', matrix), Rover(3, 3, 'E', matrix)]
+    def check_positive_integer(value):
+        try:
+            val = int(value)
+            if val > 0:
+                return val
+            else:
+                print("Please enter an integer greater than 0")
+                exit()
+        except ValueError:
+            print("Please enter a positive integer")
+            exit()
+
+    def check_at_least_zero(value):
+        try:
+            val = int(value)
+            if val >= 0:
+                return val
+            else:
+                print("Value must be at least 0")
+                exit()
+        except ValueError:
+            print("Please enter a non-negative integer or zero")
+            exit()
+
+    def check_direction(value):
+        if value.upper() in ['N', 'E', 'S', 'W']:
+            return value.upper()
+        else:
+            print("Please enter a valid direction (N, E, S or W)")
+            exit()
+
+    def check_instruction(value):
+        if value.upper() in ['L', 'R', 'M']:
+            return value.upper()
+        else:
+            print("Please enter a valid instruction (L, R or M)")
+
+    height = input("Please enter a height for the plateau: ")
+    check_positive_integer(height)
+    width = input("Please enter a width for the plateau: ")
+    check_positive_integer(width)
+    matrix = Matrix(height, width)
+
+    rovers = []
+
+    num_rovers = check_at_least_zero(input("How many rovers are there? "))
+
+    if num_rovers == 0:
+        print("No rovers to move")
+        exit()
+
+    for rover in range(num_rovers):
+        x = check_at_least_zero(input("Please enter the x coordinate for the rover: "))
+        y = check_at_least_zero(input("Please enter the y coordinate for the rover: "))
+        direction = input("Please enter the direction for the rover, one of either N, E, S or W: ").upper()
+        check_direction(direction)
+        new_rover = Rover(x, y, direction, matrix)
+        num_instructions = check_at_least_zero(input("How many instructions are there for the rover? "))
+        for _ in range(num_instructions):
+            instruction = input(("Please enter the instruction (one of either L, R or M): ").upper())
+            check_instruction(instruction)
+            new_rover.instructions.append(instruction)
+        rovers.append(new_rover)
     
-    rovers[0].instructions = ['L', 'M', 'L', 'M', 'L', 'M', 'L', 'M', 'M']
-    rovers[1].instructions = ['M', 'M', 'R', 'M', 'M', 'R', 'M', 'R', 'R', 'M']
     solution = Solution(matrix, rovers)
     solution.run()
     print(solution.get_final_positions())
